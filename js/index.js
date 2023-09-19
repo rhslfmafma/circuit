8e1a50154e4b8715dba203ec58454ee4c5b1ff68
jungsuk chae <portfoliochae@gmail.com>
2023-09-19 17:08:57 +0900

    [FIX] 키다운 구현중

--- js/index.js
+++ js/index.js
@@ -24,4 +24,17 @@ $(function() {
     //로딩애니메이션이 3초간 있다가 바로 콘텐츠가 나오게 하되, 로딩애니메이션은 
 
 
+    // 스페이스키 또는 엔터키 눌렸을 때 이벤트 핸들러
+      $(document).keydown(function(e) {
+        if (e.keyCode == 13 || e.keyCode == 32) { // 13은 엔터키, 32는 스페이스키
+          // 페이지 이동 로직을 여기에 추가          
+          $('.display01').css({
+            display:'none'
+          });
+          $('.display02').css({
+            display: 'block'
+          })
+        }
+      });    
+
 });
