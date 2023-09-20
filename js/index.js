bb16c59d98687b815c078722d309bfb3773a1e93
jungsuk chae <portfoliochae@gmail.com>
2023-09-20 18:14:53 +0900

    [ADD] 다국어 번역기능 완료

--- js/index.js
+++ js/index.js
@@ -85,23 +85,43 @@ $(function() {
 
 
   // $.getJSON('파일경로', 할일);  
-
   let  $allData = [];
   function changeLang(lang){
   $.getJSON(`./data/${lang}.json`, function(data){
             $allData = data;
             console.log($allData);
-
             let menu = $('.nav li');
-            let tt1 = $allData[0].menu.title1;
-            menu.eq(0).find('a').text(tt1);
+            let mn1 = $allData[0].menu.title1;
+            let mn2 = $allData[0].menu.title2;
+            let mn3 = $allData[0].menu.title3;
+            let mn4 = $allData[0].menu.title4;
+
+            let displayprompt = $('.whitedisplay');
+            let tt1 = $allData[1].title;
+            let tt2 = $allData[2].title;
+            console.log(tt1);
+
+            let tt3 = $allData[3].title;
+            let tt4 = $allData[4].title;
+            let tt5 = $allData[5].title;
+            
+            menu.eq(0).find('a').text(mn1);
+            menu.eq(1).find('a').text(mn2);
+            menu.eq(2).find('a').text(mn3);
+            menu.eq(3).find('a').text(mn4);
+            
+            displayprompt.eq(1).find('.speach_buble p').html(tt1);
+            displayprompt.eq(2).find('.speach_buble p').html(tt2);
+            displayprompt.eq(3).find('.speach_buble p').html(tt3);
+            displayprompt.eq(4).find('.speach_buble p').html(tt4);
+
+
+
 
         });
-       
-
-    }
-
+  }
     changeLang('kor');
+
 
     let btn = $('.lang');
     
