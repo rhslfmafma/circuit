993a028000e8d51c6b773fb10c56cc0e0a9064e7
jungsuk chae <portfoliochae@gmail.com>
2023-09-27 17:59:41 +0900

    [FIX] 오류해결

--- js/intro.js
+++ js/intro.js
@@ -1,149 +1,93 @@
 $(function() {  
- 
-
-      //언어버튼 클릭 시 드롭다운 슬라이드 다운
-      let languagebtn = $('.languagebtn');
-      let dropdownMenu = $('.dropdown-menu');
-    
+  //언어버튼 클릭 시 드롭다운 슬라이드 다운
+  let languagebtn = $('.languagebtn');
+  let dropdownMenu = $('.dropdown-menu');    
       languagebtn.click(function(){
-        dropdownMenu.slideToggle();
-      })
-    
-    
-    let speach_bubble = $('.intro .speach_bubble ');
-    
-    speach_bubble.click(function(){
+      dropdownMenu.slideToggle();
+      })       
+  let speach_bubble = $('.intro .speach_bubble ');    
+      speach_bubble.click(function(){
       $(this).find(p).eq(0).hide();
-      $(this).find(p).eq(1).show();
-      
+      $(this).find(p).eq(1).show();     
     })
-  
 
-   
+  // $.getJSON('파일경로', 할일);  
+  let  $allData = [];
+  let spanElements = $('.txtgroup li');
+  spanElements.hide();
+  let index = 0;
 
-
-
-// $.getJSON('파일경로', 할일);  
-let  $allData = [];
-let spanElements = $('.txtgroup span');
-let index = 0;
-function changeLang(lang){
-$.getJSON(`./data/${lang}.json`, function(data){
-          $allData = data;
-          console.log($allData);
-          let menu = $('.nav li');
-          let mn1 = $allData[0].menu.title1;
-          let mn2 = $allData[0].menu.title2;
-          let mn3 = $allData[0].menu.title3;
-          let mn4 = $allData[0].menu.title4;
-
-          let displayprompt = $('.whitedisplay .speach_bubble');
-          let tt1 = $allData[1].title;
-          let tt2 = $allData[2].title;
-          let tt3 = $allData[3].title;
-          console.log(tt1);
-
-          let tt4 = $allData[4].title;
-          let tt5 = $allData[5].title;
-          let tt6 = $allData[6].title;
-          
-          menu.eq(0).find('a').text(mn1);
-          menu.eq(1).find('a').text(mn2);
-          menu.eq(2).find('a').text(mn3);
-          menu.eq(3).find('a').text(mn4);
-          
-          displayprompt.find('p').eq(0).html(tt1);
-          displayprompt.find('p').eq(1).html(tt2);
-          displayprompt.find('p').eq(2).html(tt3);
-          displayprompt.find('p').eq(3).html(tt4);
-          displayprompt.find('p').eq(4).html(tt5);
-          displayprompt.find('p').eq(5).html(tt6);
-          
-
-
-
-      });
-}
-
-
-
-
-
-
-
-          // 타이핑 애니메이션 실행
-          function typeText($element, text) {
-            let index = 0;
-            let interval = setInterval(function() {
-              $element.append(text[index]);
-              index++;
-              if (index === text.length) {
-                clearInterval(interval);
-              }
-            }, 300);
-          }
-     
-  function typeNext() {
-            if (index < spanElements.length) {
-              let text = spanElements.eq(index).text();
-              spanElements.eq(index).empty();
-              typeText(spanElements.eq(index),text);
-              index++;
-            }
-          }
-
-          setInterval(() => {
-            typeNext(index)
-            index++;     
-            if (index === spanElements.find('li').length) {
+        // 타이핑 애니메이션 실행
+        function typeText($element, text) {
+          let index = 0;
+          let interval = setInterval(function() {
+            $element.append(text[index]);
+            index++;
+            if (index === text.length) {
               clearInterval(interval);
-            }       
-          2000}, interval);
-
-  
-  function changeLang(lang){
-  $.getJSON(`./data/${lang}.json`, function(data){
-            $allData = data;
-            console.log($allData);
-            let menu = $('.nav li');
-            for(let i = 0; i<menu.length; i++) {
-              menu.eq(i).find('a').text($allData[0].menu['title'+(i+1)]);
             }
+          }, 300);
+        }
 
-            //displaypromptred 제목 업데이트
-            let displaypromptred = $('.intro .txtgroup');
-            for(let i = 7; i<=20; i++) {
-            displaypromptred.find('span').eq(i-7).html($allData[i].title);
-            }
+        function typeNext(index) {
+          if (index < spanElements.length) {
+            let text = spanElements.eq(index).text();
+            spanElements.eq(index).empty();
+            typeText(spanElements.eq(index),text);
+            index++;
+          }
+        }
 
+        let typeanimation  = setInterval(() => {
+          spanElements.eq(index).show();
+          typeNext(index);
+          index++;     
+          if (index === spanElements.length) {
+            clearInterval(typeanimation);
+          }
+        }, 2000);
         
+ 
+  
+        function changeLang(lang){
+          $.getJSON(`./data/${lang}.json`, function(data){
+                    $allData = data;
+                    console.log($allData);
+                    let menu = $('.nav li');
+
+          for(let i = 0; i<menu.length; i++) {
+            menu.eq(i).find('a').text($allData[0].menu['title'+(i+1)]);
+          }
+
+          //displaypromptred 제목 업데이트
+          let displaypromptred = $('.intro .txtgroup');
+          for(let i = 7; i<=20; i++) {
+          displaypromptred.find('li').eq(i-7).html($allData[i].title);
+          }
+
+      
+        typeNext();
+
+
+        //다음 span 타이핑 시작하기 위한 이벤트리스너
+        spanElements.on('animationEnd webkitAnimationEnd oAnimationEnd MSAnimationEnd', function() {
           typeNext();
-
-
-          //다음 span 타이핑 시작하기 위한 이벤트리스너
-          spanElements.on('animationEnd webkitAnimationEnd oAnimationEnd MSAnimationEnd', function() {
-            typeNext();
-          });
         });
-    }
+      });
+  }
 
 
-    changeLang('kor');
-
-
-
+  changeLang('kor');
 
 
     
-    
 
-    let btn = $('.lang');
+  let btn = $('.lang');
     
-    btn.click(function(){      
-    let target = $(this).attr('data-lang');
-    changeLang(target);
-    })
-
+  btn.click(function(){      
+  let target = $(this).attr('data-lang');
+  changeLang(target);
+  });
 
 
   /* intro.html */
